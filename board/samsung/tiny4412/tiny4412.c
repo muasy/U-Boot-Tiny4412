da9faf9a11d15fcb6e1028a65ec8d018f7b2a7b0
SY <1530454315@qq.com>
2017-06-11 19:41:56 +0800

    2017-6-11 19:41:44[增加USB OTG驱动]

--- board/samsung/tiny4412/tiny4412.c
+++ board/samsung/tiny4412/tiny4412.c
@@ -3,15 +3,22 @@
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
-
 #include <common.h>
-#include <asm/io.h>
+#include <asm/arch/pinmux.h>
+#include <asm/arch/power.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/gpio.h>
 #include <asm/gpio.h>
 #include <asm/arch/cpu.h>
-#include <asm/arch/mmc.h>
-#include <asm/arch/periph.h>
-#include <asm/arch/pinmux.h>
+#include <dm.h>
+#include <power/pmic.h>
+#include <power/regulator.h>
+#include <power/max77686_pmic.h>
+#include <errno.h>
+#include <mmc.h>
 #include <usb.h>
+#include <usb/dwc2_udc.h>
+#include <samsung/misc.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -20,14 +27,38 @@ u32 get_board_rev(void)
 	return 0;
 }
 
+static void board_gpio_init(void)
+{
+	;
+}
+
 int exynos_init(void)
+{
+	board_gpio_init();
+
+	return 0;
+}
+
+#ifdef CONFIG_USB_GADGET
+static int s5pc210_phy_control(int on)
 {
 	return 0;
 }
 
+struct dwc2_plat_otg_data s5pc210_otg_data = {
+	.phy_control	= s5pc210_phy_control,
+	.regs_phy	= EXYNOS4X12_USBPHY_BASE,
+	.regs_otg	= EXYNOS4X12_USBOTG_BASE,
+	.usb_phy_ctrl	= EXYNOS4X12_USBPHY_CONTROL,
+	.usb_flags	= PHY0_SLEEP,
+};
+#endif
+
 int board_usb_init(int index, enum usb_init_type init)
 {
-	return 0;
+	debug("USB_udc_probe\n");
+	
+	return dwc2_udc_probe(&s5pc210_otg_data);
 }
 
 #ifdef CONFIG_BOARD_EARLY_INIT_F
